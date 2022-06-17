facfa0b1d3d2d42c1e4254cb484579165c6e08e3
Niall Finlay <niall.finlay@gmail.com>
2022-06-17 11:26:40 +0100

    module1

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -1,6 +1,6 @@
 import os
-import csv
 import glob
+import csv
 
 def load_sensor_data():
   sensor_data = []
@@ -9,7 +9,7 @@ def load_sensor_data():
   for sensor_file in sensor_files:
     with open(sensor_file) as data_file:
       data_reader = csv.DictReader(data_file, delimiter=',')
-      for row in data_file:
+      for row in data_reader:
         sensor_data.append(row)
 
   return sensor_data

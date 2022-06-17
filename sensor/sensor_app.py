5714a35284222461353ee2957684eb7c03029268
Niall Finlay <niall.finlay@gmail.com>
2022-06-17 11:23:32 +0100

    module1

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,11 +1,14 @@
 # Runner script for all modules
 
+from load_data import load_sensor_data
 
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
 data = []
 print("Sensor Data App")
+data = load_sensor_data()
+print('Loaded records: {}'.format(len(data)))
 ##############################
 
 # Module 1 code here:
